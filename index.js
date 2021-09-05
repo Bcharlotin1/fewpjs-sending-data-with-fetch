39e8d18249dfbe54b870f4a35588615827810d25
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-05 15:58:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    submitData();
+});
+
+
+let formData = {
+    name: "Bruce",
+    email:"bruce@gotham.com"
+}
+
+let configObj = {
+    // method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+}
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    .catch(function(error) {
+        const errorMessage = document.createElement("h2")
+        errorMessage.innerHTML = `${error.message}`;
+        console.log(error.message);
+
+        document.querySelector("body").append(errorMessage)
+        
+
+    })
+}
